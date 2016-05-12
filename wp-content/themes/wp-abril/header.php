c8b29f0bbc9b5b2b5e95365bba8622202d09e80c
Rafael Milani Babrosa <rafaelmilanib@gmail.com>
2016-05-12 01:38:58 -0300

    adicionando bootstrap files

--- wp-content/themes/wp-abril/header.php
+++ wp-content/themes/wp-abril/header.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <!-- carregando titulo dinamicamente do wordpress -->
-  <title><?php  wp_title(); ?></title>
-  <!-- carregando style.css da raiz do tema caos exista -->
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+  	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- carregando titulo dinamicamente do wordpress -->
+  	<title><?php  wp_title(); ?></title>
+  	<!-- carregando style.css da raiz do tema caos exista -->
+  	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+  	<link href="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- carregando scripts dinamicamente no head pelo wordpress -->
-  <?php wp_head(); ?>
+  	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+  	<!-- carregando scripts dinamicamente no head pelo wordpress -->
+  	<?php wp_head(); ?>
 </head>
 <body>
-
-
-<h2>Teste</h2>
