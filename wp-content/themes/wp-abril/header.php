a3c78f3fa27ada36a7a7d7baadfb3b7beb5dd98c
Rafael Milani Babrosa <rafaelmilanib@gmail.com>
2016-05-12 01:19:26 -0300

    bug corrigido

--- wp-content/themes/wp-abril/header.php
+++ wp-content/themes/wp-abril/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <!-- carregando titulo dinamicamente do wordpress -->
-  <title><?php  wh_title(); ?></title>
+  <title><?php  wp_title(); ?></title>
   <!-- carregando style.css da raiz do tema caos exista -->
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 
