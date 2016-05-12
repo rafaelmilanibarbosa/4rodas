ceaa4bf1dfb202d97b92ea07254ff85b756cb0d3
Rafael Milani Babrosa <rafaelmilanib@gmail.com>
2016-05-12 01:59:46 -0300

    correcao footer dorectory

--- wp-content/themes/wp-abril/footer.php
+++ wp-content/themes/wp-abril/footer.php
@@ -6,9 +6,9 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/js/jquery.min.js"><\/script>')</script>
-    <script src="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/js/bootstrap.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.min.js"><\/script>')</script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
