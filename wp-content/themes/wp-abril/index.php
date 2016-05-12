fed635876a329740309011f6b1c3f13dcd03ec40
Rafael Milani Babrosa <rafaelmilanib@gmail.com>
2016-05-12 02:24:35 -0300

    fix background

--- wp-content/themes/wp-abril/index.php
+++ wp-content/themes/wp-abril/index.php
@@ -10,7 +10,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><img class="logo"></a>
+      <a class="navbar-brand" href="#"><div class="logo"></div></a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
