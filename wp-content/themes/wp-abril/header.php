9eff8013a96dd6ef61bebe45212121b10e8097be
Rafael Milani Babrosa <rafaelmilanib@gmail.com>
2016-05-12 01:56:14 -0300

    adicionando variave template dorectory

--- wp-content/themes/wp-abril/header.php
+++ wp-content/themes/wp-abril/header.php
@@ -8,18 +8,18 @@
   	<title><?php  wp_title(); ?></title>
   	<!-- carregando style.css da raiz do tema caos exista -->
   	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
-  	<link href="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/css/bootstrap.min.css" rel="stylesheet">
+  	<link href="<?php bloginfo('template_directory'); ?>/assets/css/bootstrap.min.css" rel="stylesheet">
 
   	<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="http://digitalsea.com.br/4rodas/wp-content/themes/wp-abril/assets/js/ie-emulation-modes-warning.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    
+
   	<!-- carregando scripts dinamicamente no head pelo wordpress -->
   	<?php wp_head(); ?>
 </head>
