ac1e9d6fef7d6b07874dc0731400c036ad12503b
Rafael Barbosa <rafael.milani@live.com>
2016-05-30 10:14:26 -0300

    fixando erro query

--- wp-content/plugins/4rodas/estoque-create.php
+++ wp-content/plugins/4rodas/estoque-create.php
@@ -8,14 +8,14 @@ $preco 		= $_POST["preco"];
 //insert
 if(isset($_POST['insert'])){
 	global $wpdb;
-	/*$wpdb->insert(
+	$wpdb->insert(
 		'Produto', //table
 		array('nome' => $nome, 'descricao' => $descricao, 'preco' => $preco) //data		
-	);*/
+	);
 	
-	$insert =  $wpdb->query("INSERT INTO `Produto`(`nome`, `descricao`, `preco`) VALUES ('$nome', '$descricao', '$preco')");
-	print_r($insert);
-	$message.="Produto adicionado t";
+/*	$insert =  $wpdb->query("INSERT INTO `Produto`(`nome`, `descricao`, `preco`) VALUES ('$nome', '$descricao', '$preco')");
+	print_r($insert);*/
+	$message.="Produto adicionado";
 }
 ?>
 <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/4rodas/style-admin.css" rel="stylesheet" />
